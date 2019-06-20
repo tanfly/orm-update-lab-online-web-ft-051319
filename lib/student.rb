23ecc1f425e4708fb0c42eb3e1afa488c6ee99fb
Stella McEwen <mcewen.stella@gmail.com>
2019-06-20 19:29:25 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -79,8 +79,4 @@ class Student
     DB[:conn].execute(sql, self.name, self.grade, self.id) 
   end 
   
-  def self.new_from_db(row)
-    dog = Dog.new(id: row[0], name: row[1], breed: row[2])
-  end 
-  
 end
